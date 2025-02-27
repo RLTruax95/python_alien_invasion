351f24aeb794a8cc90d9018f725afd028b35aeaa
Ryan Truax <Truax53@live.com>
2025-02-26 22:31:45 -0500

    Made changes to accept the speed setting added to the settings class

--- Alien_Invasion_Game/ship.py
+++ Alien_Invasion_Game/ship.py
@@ -7,6 +7,7 @@ class Ship:
 		"""Initialize the ship and set its starting position"""
 		self.screen = ai_game.screen
 		self.screen_rect = ai_game.screen.get_rect()
+		self.settings = ai_game.settings
 
 		#Load the ship image and get its rect.
 		self.image = pygame.image.load('Images/spaceship.bmp')
@@ -15,16 +16,22 @@ class Ship:
 		#Start each new ship at the bottom center of the screen
 		self.rect.midbottom = self.screen_rect.midbottom
 
+		#Store the horizontal position as a decimal value
+		self.x = float(self.rect.x)
+
 		#Movement flags
 		self.moving_right = False
 		self.moving_left = False
 
 	def update(self):
 		"""Update the ships position based on the movement flag"""
-		if self.moving_right:
-			self.rect.x += 1
-		elif self.moving_left:
-			self.rect.x -= 1
+		if self.moving_right and self.rect.right < self.screen_rect.right:
+			self.rect.x += self.settings.ship_speed
+		if self.moving_left and self.rect.left > 0:
+			self.rect.x -= self.settings.ship_speed
+
+		#Update rect object from self.x
+		# self.rect.x = self.x  #This command is supposed to be in the program, but prevents the ship from moving
 
 	def blitme(self):
 		"""Draw the ship at its current location"""
