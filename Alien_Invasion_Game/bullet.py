878e44ee25b4ba24460429a43bf456c7fe4b0f03
Ryan Truax <Truax53@live.com>
2025-03-06 22:23:06 -0500

    Created a bullet class ot handle the initialization and updating of the bullets on the screen

--- Alien_Invasion_Game/bullet.py
+++ Alien_Invasion_Game/bullet.py
@@ -7,14 +7,25 @@ class Bullet(Sprite):
     def __init__(self, ai_game):
         """Initialize the bullet and set its starting position"""
         super().__init__()
-        self.screen_width = ai_game.screen
+        self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.color = self.settings.bullet_color
 
         #Create a bullet rectangle at (0,0) and then set the correct position
-        self.rect = pygame.Rect(0, 0, self.settings.screen_width,
-                                self.settings.screen_height)
-        self.rect.midtop = ai_game.ship_rect.midtop
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
+                                self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
 
         #Store the bullets position as a decimal value
         self.y = float(self.rect.y)
+
+    def update(self):
+        """Move the bullets up the screen"""
+        #Update the decimal position of the bullet
+        self.y -= self.settings.bullet_speed
+        #update the rect position
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Draw the bullet on the screen"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
