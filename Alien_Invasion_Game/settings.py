2d8ad228722aea8f21e673602b2eac39bf475882
Ryan Truax <Truax53@live.com>
2025-03-09 00:07:38 -0500

    Add setting for the number of ships a player gets to use

--- Alien_Invasion_Game/settings.py
+++ Alien_Invasion_Game/settings.py
@@ -7,7 +7,6 @@ class Settings:
 		self.screen_width = 1200
 		self.screen_height = 800
 		self.bg_color = (230, 230, 230)
-		self.ship_speed = 1.0
 
 		#Bullet settings
 		self.bullet_speed = 0.8
@@ -21,3 +20,7 @@ class Settings:
 		self.fleet_drop_speed = 10
 		#Fleet direction 1 = right, -1 = left
 		self.fleet_direction = 1
+
+		#Ship settings
+		self.ship_limit = 3
+		self.ship_speed = 1.0
