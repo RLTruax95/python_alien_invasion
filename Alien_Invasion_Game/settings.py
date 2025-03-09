a7a9a369afa2dd8440940aab2ec9af7c7f34b4b5
Ryan Truax <Truax53@live.com>
2025-03-08 23:23:29 -0500

    Added the settings for the aliens movement

--- Alien_Invasion_Game/settings.py
+++ Alien_Invasion_Game/settings.py
@@ -15,3 +15,9 @@ class Settings:
 		self.bullet_height = 15
 		self.bullet_color = (60, 60, 60)
 		self.bullets_allowed = 3
+
+		#Alien settings
+		self.alien_speed = 0.25
+		self.fleet_drop_speed = 10
+		#Fleet direction 1 = right, -1 = left
+		self.fleet_direction = 1
