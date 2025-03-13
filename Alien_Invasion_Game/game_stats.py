ee97a9fae8208cd32162be7f38e6f9c316dcb96e
Ryan Truax <Truax53@live.com>
2025-03-13 18:45:25 -0400

    Added variables for tracking the game score

--- Alien_Invasion_Game/game_stats.py
+++ Alien_Invasion_Game/game_stats.py
@@ -6,9 +6,13 @@ class GameStats:
         self.reset_stats()
         self.game_active = True
 
+        #High scores should not get reset
+        self.high_score = 0
+
         #Start game in an inactive state
         self.game_active = False
 
     def reset_stats(self):
         """Initialize statistics that can change during the game"""
         self.ships_left = self.settings.ship_limit
+        self.score = 0
