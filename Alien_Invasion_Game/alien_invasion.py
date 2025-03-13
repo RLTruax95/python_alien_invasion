7a9f572e0a8d74ce2325daeda78b2be243473783
Ryan Truax <Truax53@live.com>
2025-03-13 17:46:29 -0400

    Add features to create and draw a button to start the game

--- Alien_Invasion_Game/alien_invasion.py
+++ Alien_Invasion_Game/alien_invasion.py
@@ -7,6 +7,7 @@ from ship import Ship
 from bullet import Bullet
 from alien import Alien
 from game_stats import GameStats
+from button import Button
 
 class AlienInvasion:
 	"""Class for managing the game assets"""
@@ -33,6 +34,7 @@ class AlienInvasion:
 		self.aliens = pygame.sprite.Group()
 
 		self._create_fleet()
+		self.play_button = Button(self, "Play")
 # --------------------------------------------------------------------------
 	def run_game(self):
 		"""Start the main loop for the game"""
@@ -140,6 +142,10 @@ class AlienInvasion:
 		for bullet in self.bullets.sprites():
 			bullet.draw_bullet()
 		self.aliens.draw(self.screen)
+
+		#Draw the play button if the game is inactive
+		if not self.stats.game_active:
+			self.play_button.draw_button()
 
 		# Makes the most recent screen visible
 		pygame.display.flip()
